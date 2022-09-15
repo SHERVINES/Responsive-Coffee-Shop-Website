f9a40afe1165094724cf5353b1f7d59029d23f0c
shervin <shervin.ses@gmail.com>
2022-09-15 19:11:57 +0430

    searchForm and menu and cart items js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let navabar = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick = () => {
+  navabar.classList.toggle("active");
+  cartItem.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+let cartItem = document.querySelector(".cart-items-container");
+document.querySelector("#cart-btn").onclick = () => {
+    cartItem.classList.toggle("active");
+    navabar.classList.remove("active");
+    searchForm.classList.remove("active");
+};
+let searchForm = document.querySelector(".search-from");
+document.querySelector("#search-btn").onclick = () => {
+    searchForm.classList.toggle("active");
+    navabar.classList.remove("active");
+    cartItem.classList.remove("active");
+};
+
+window.onscroll() = ()=>{
+    navabar.classList.remove("active");
+    cartItem.classList.remove("active");
+    searchForm.classList.remove("active");
+}
